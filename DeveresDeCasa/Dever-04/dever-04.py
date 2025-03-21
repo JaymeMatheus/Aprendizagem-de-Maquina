02fec275273cbaf068421dd9ef22c660524cce6b
Jayme Matheus <jaymematheus.2020@gmail.com>
2025-03-21 20:59:15 -0300

    Entrega Dever-04

--- DeveresDeCasa/Dever-04/dever-04.py
+++ DeveresDeCasa/Dever-04/dever-04.py
@@ -3,20 +3,17 @@ import random
 
 frutas = ["maçã", "banana", "laranja", "uva", "maçã", "melão", "mamão", "banana"]
 
-set_frutas = list(set(frutas))
-
 quantidades = {}
+
 for fruta in frutas:
     quantidade = random.randint(0, 100)
-    quantidades[fruta] = quantidades.get(fruta, 0) + quantidade
-
+    nova_quantidade = min(quantidades.get(fruta, 0) + quantidade, 100)
+    quantidades[fruta] = nova_quantidade
 
 with open("minhas_frutas.txt", "w", encoding="utf-8-sig") as file:
     for fruta, quantidade in quantidades.items():
         file.write(f"{fruta},{quantidade}\n")
 
-
 dados = pd.read_csv("minhas_frutas.txt", names=["Fruta", "Quantidade"], encoding="utf-8-sig")
-dados = dados.groupby("Fruta", as_index=False).sum()
 
 print(dados)
